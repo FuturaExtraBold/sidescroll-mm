1540c31da20b1110b108066785b821e230d0e911
Ben Hays <ben@kajabi.com>
2019-08-12 15:51:25 -0700

    Fixed offset logic for triggering actions on scroll.

--- source/javascripts/modules/_online_courses.js
+++ source/javascripts/modules/_online_courses.js
@@ -1,26 +1,26 @@
 (function($) {
+
   let $window = $(window);
-  let windowW = $window.outerWidth();
+  let windowWidth = $window.outerWidth();
   let $oc = $(".online-courses");
 
-  console.log("windowW / 100:", windowW / 100);
-
   let opacityPercent = 0;
-  let threshold = 0.75;
-  let modifier = 0;
-  let ol = 0;
-  let wt = 0;
+  let windowStart = 0.75;
+  let windowEnd = 0.25;
+  let windowDiff = windowStart - windowEnd;
+  let offsetLeft = $oc.offset().left;
+  let range = 0;
 
   $window.on("scroll", () => {
-    ol = $oc.offset().left;
-    wt = windowW * threshold;
-    if (ol < wt) {
-      opacityPercent = (1 + modifier) - (Math.floor(ol * (100 / wt))) / 100;
-    } else {
-      opacityPercent = 0;
+    offsetLeft = $oc.offset().left;
+    if (offsetLeft < windowWidth * windowStart && offsetLeft > windowWidth * windowEnd) {
+      console.log("within range!");
+      opacityPercent = (1 + windowDiff) - (offsetLeft * (100 / (windowDiff * windowWidth))) / 100;
     }
-    if (opacityPercent >= 1) opacityPercent = 1;
-    // console.log(opacityPercent);
+    if (offsetLeft >= windowWidth * windowStart) opacityPercent = 0;
+    if (offsetLeft <= windowWidth * windowEnd) opacityPercent = 1;
+    console.log(opacityPercent);
     TweenMax.set($oc, { opacity: opacityPercent });
   });
+
 })(jQuery);
